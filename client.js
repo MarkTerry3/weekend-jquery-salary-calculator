3461a0959577fb6c804da7051d1d71c4a8cae210
Mark Terry <mark.terry93@gmail.com>
2022-01-28 17:02:14 -0600

    made employee object

--- client.js
+++ client.js
@@ -2,6 +2,15 @@ $(document).ready(readyNow);
 
 console.log('js working');
 
+let employees = {
+    firstName: $('#firstNameInput'),
+    lastName: $('#lastNameInout'),
+    id: $('#idInput'),
+    title: $('#titleInput'),
+    annualSalary: $('#annualSalaryInput')
+
+}
+
 function readyNow() {
     console.log('JQuery working');
     $('#submitButton').on('click', submitButton);
@@ -9,11 +18,17 @@ function readyNow() {
 
 
 function submitButton() {
-    $('#tdFirstName').text('#firstNameInput');// isnt working need to revisit
+    $('#tdFirstName').text ($('#firstNameInput').val()); // isnt working need to revisit
 }// end submitButton
 
 
+//store these in an object, then push them to an array, which i did in the employees object
 
+
+
+//append to the table data to have multiple rows .append(<td>firstname</td>)
+
+//monthly total will require dividing. divide by 12 for 12 months
 
 
 // line 12 is where you left off, cant figure out how to make the input be sent to the table
