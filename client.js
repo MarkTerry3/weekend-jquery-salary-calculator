c3707b4a34b1688594d1f03a5259f3376a46e5ce
Mark Terry <mark.terry93@gmail.com>
2022-01-28 15:55:08 -0600

    tables are almost done, still needs work

--- client.js
+++ client.js
@@ -4,5 +4,16 @@ console.log('js working');
 
 function readyNow() {
     console.log('JQuery working');
-    
+    $('#submitButton').on('click', submitButton);
 }// end readyNow
+
+
+function submitButton() {
+    $('#tdFirstName').text('#firstNameInput');// isnt working need to revisit
+}// end submitButton
+
+
+
+
+
+// line 12 is where you left off, cant figure out how to make the input be sent to the table
