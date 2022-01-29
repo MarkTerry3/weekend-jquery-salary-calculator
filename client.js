7b7c7c395323855365dd6834fa648457a45ff06d
Mark Terry <mark.terry93@gmail.com>
2022-01-29 13:14:33 -0600

    have employeesIn being pushed into the employeesOut array

--- client.js
+++ client.js
@@ -2,14 +2,7 @@ $(document).ready(readyNow);
 
 console.log('js working');
 
-let employees = {
-    firstName: $('#firstNameInput'),
-    lastName: $('#lastNameInout'),
-    id: $('#idInput'),
-    title: $('#titleInput'),
-    annualSalary: $('#annualSalaryInput')
-
-}
+let employeesOut = [];
 
 function readyNow() {
     console.log('JQuery working');
@@ -18,21 +11,46 @@ function readyNow() {
 
 
 function submitButton() {
-    $('#tdFirstName').text ($('#firstNameInput').val());
-    $('#tdLastName').text ($('#lastNameInput').val());
-    $('#tdID').text ($('#idInput').val());
-    $('#tdTitle').text ($('#titleInput').val());
-    $('#tdAnnualSalary').text ($('#annualSalaryInput').val());  
+    let employeesIn = {
+    firstName: $('#firstNameInput').val(),
+    lastName: $('#lastNameInput').val(),
+    id: $('#idInput').val(),
+    title: $('#titleInput').val(),
+    annualSalary: $('#annualSalaryInput').val()
+}//end employeesIN
+
+    //push employees to array
+    employeesOut.push(employeesIn);
+
+    $('#tdFirstName').append (`<tr><td> ${employeesIn.firstName} </td></tr>`);
+    $('#tdLastName').append (`<tr><td> ${employeesIn.lastName} </td></tr>`);
+    $('#tdID').append (`<tr><td> ${employeesIn.id} </td></tr>`);
+    $('#tdTitle').append (`<tr><td> ${employeesIn.title} </td></tr>`);
+    $('#tdAnnualSalary').append (`<tr><td> ${employeesIn.annualSalary} </td></tr>`);
+
+    
+
 }// end submitButton
 
+function monthlyCosts() {
+    //take in employee ANNUAL salary
+}//end monthlyCosts
 
-//store these in an object, then push them to an array, which i did in the employees object
-
-
-
-//append to the table data to have multiple rows .append(<td>firstname</td>)
+//store these in an object, then push them to an array
 
 //monthly total will require dividing. divide by 12 for 12 months
 
 
-// line 12 is where you left off, cant figure out how to make the input be sent to the table
+// ## Topics Covered
+// - JavaScript
+// - jQuery - Selectors, append, and event handling
+
+// ## Assignment
+
+// - DONE The application should have an input form that collects _employee first name, last name, ID number, job title, annual salary_.
+
+// A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields. Using the stored information, calculate monthly costs and append this to the to DOM. If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
+
+// Create a delete button that removes an employee from the DOM. For Base mode, it does **not** need to remove that Employee's salary from the reported total.
+
+//stretch goal of color blocks for the delete button
